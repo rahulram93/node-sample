b6bc48a7df49e14ce7ee838bce05a29e6257ed72
rahul <rahulram9993@gmail.com>
2018-12-16 14:04:57 +0530

    CH 03 completed

--- meadowlark.js
+++ meadowlark.js
@@ -6,14 +6,25 @@ var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' }
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.static(__dirname + '/public'));
+
 app.set('port', process.env.port || 3000);
 
 app.get('/', function(req, res){
   res.render('home');
 });
 
+
+var fortunes = ["Conquer your fears or they will conquer you",
+                "Rivers need Springs.",
+                "Do not fear what you don't know",
+                "you will have a pleasant surprise.",
+                "whenever possible, keep it simple."
+];
+
 app.get('/about', function(req, res){
-  res.render('about');
+  var randomFortune = fortunes[Math.floor( Math.random() * fortunes.length )];
+  res.render('about', {fortune: randomFortune});
 });
 
 // Custom 404 page
